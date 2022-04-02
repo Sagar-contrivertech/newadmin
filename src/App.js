aef52aa55fbd0954b0167654cf95df381dc3a3e8
Vatsal Soni <vatsal.soni@contrivertech.com>
2022-04-02 17:05:23 +0530

    login page + axios page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import Home from './Components/Home/Home';
+import React from "react";
+import Home from "./Components/Home/Home";
+import Auth from "./Components/Auth/Auth";
+
+import { HashRouter, Routes, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home/>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Auth} />
+          <Route exact path="/dashboard" component={Home} />
+
+          {/* <Route exact path="/dashboard" /> */}
+            {/* <Protected cmp={Home} /> */}
+          {/* </Route> */}
+        </Switch>
+      </HashRouter>
     </>
   );
 }
