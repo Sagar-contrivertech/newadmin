e936be0212c2e8fd42eed3c73c4ea4e62bc9dbc8
Sagar-contrivertech <sagar@contrivertech.com>
2022-04-02 17:53:06 +0530

    proteced routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Components/Home/Home";
 import Auth from "./Components/Auth/Auth";
 
 import { HashRouter, Routes, Route, Switch } from "react-router-dom";
+import Protected from "./Components/Protected/Protected";
 
 function App() {
   return (
@@ -10,11 +11,10 @@ function App() {
       <HashRouter>
         <Switch>
           <Route exact path="/" component={Auth} />
-          <Route exact path="/dashboard" component={Home} />
-
-          {/* <Route exact path="/dashboard" /> */}
-            {/* <Protected cmp={Home} /> */}
-          {/* </Route> */}
+          <Route exact path="/dashboard" >
+            <Protected cmp={Home}/>
+          </Route>
+          
         </Switch>
       </HashRouter>
     </>
